cdb12e2d1760c3db0c5971a81db410ab183a0447
Ernestas <e.konopliov@gmail.com>
2019-05-29 21:50:04 +0300

    Fist commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,215 @@
+package com.example.myapplication;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    final MediaPlayer mediaPlayer1 = MediaPlayer.create(this, R.raw.no1);
+    final MediaPlayer mediaPlayer2 = MediaPlayer.create(this, R.raw.no2);
+    final MediaPlayer mediaPlayer3 = MediaPlayer.create(this, R.raw.no3);
+    final MediaPlayer mediaPlayer4 = MediaPlayer.create(this, R.raw.no4);
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
+    String deviceName = "HC-06";
+
+    Button btnPlay = findViewById(R.id.btnPlay);
+    Button btnConnect = findViewById(R.id.btnConnect);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                letRev();
+            }
+        });
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    findBT();
+                    openBT();
+                } catch (IOException e){
+                    Toast.makeText(MainActivity.this,"Exeption in MainActivity btnConnectListener",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    private void letRev(){
+        switch (getRandomNumberInRange(1,4)){
+            case 1:  mediaPlayer1.start();  break;
+
+            case 2:  mediaPlayer2.start();  break;
+
+            case 3:  mediaPlayer3.start();  break;
+
+            default: mediaPlayer4.start();  break;
+        }
+    }
+
+    private int getRandomNumberInRange(int min, int max) {
+
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
+    }
+
+    void analyzeReceivedData(String data){
+
+        System.out.println("Received data: " + data);
+    }
+
+    void findBT()
+    {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null)
+        {
+            Toast.makeText(this,"No bluetooth adapter available",Toast.LENGTH_SHORT).show();
+        }
+
+        if(!mBluetoothAdapter.isEnabled())
+        {
+            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetooth, 0);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 0)
+        {
+            for(BluetoothDevice device : pairedDevices)
+            {
+                if(device.getName().equals(deviceName))
+                {
+                    mmDevice = device;
+                    break;
+                }
+            }
+        }
+        Toast.makeText(this,"Bluetooth device found",Toast.LENGTH_SHORT).show();
+    }
+
+    void openBT() throws IOException
+    {
+        UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+        mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+        mmSocket.connect();
+        mmOutputStream = mmSocket.getOutputStream();
+        mmInputStream = mmSocket.getInputStream();
+
+        beginListenForData();
+
+        Toast.makeText(this,"Bluetooth connected",Toast.LENGTH_SHORT).show();
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            analyzeReceivedData(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
+    }
+
+    void sendData(String msg) throws IOException
+    {
+        msg += "\n";
+        mmOutputStream.write(msg.getBytes());
+        Toast.makeText(this,"Sent",Toast.LENGTH_SHORT).show();
+    }
+
+    void closeBT() throws IOException
+    {
+        stopWorker = true;
+        mmOutputStream.close();
+        mmInputStream.close();
+        mmSocket.close();
+        Toast.makeText(this,"Bluetooth disconnected",Toast.LENGTH_SHORT).show();
+    }
+}
+
